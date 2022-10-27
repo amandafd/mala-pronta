5e8b3759d651165ec5d115ffde19eb2e26d96c5f
amandafd <amanda_dionisio@hotmail.com>
2022-10-27 02:06:07 -0300

    feat: adicionando espaçamentos das perguntas

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,4 @@
+/*nome*/
 var nome = document.querySelector('#nome');
 var capturarNome = document.querySelector('#btn__proximo--nome');
 
@@ -8,7 +9,21 @@ capturarNome.addEventListener('click', function(e) {
 });
 
 /*lavarRoupa*/
+/*var lavarRoupa = document.querySelector('input[name="lavarRoupa"]:checked').value;
+var capturarlavarRoupa = document.querySelector('#btn__proximo--lavarRoupa');
 
+
+capturarlavarRoupa.addEventListener('click', function(e) {
+    if(lavarRoupa === "não"){
+        alert("Você não precisa lavar roupa");
+    } else if(lavarRoupa === "sim") {
+        alert("Você precisa lavar roupa");
+    }
+
+    e.preventDefault();
+});*/
+
+/*duracaoTotal*/
 var duracaoTotal = document.querySelector('#duracaoTotal');
 var capturarDuracaoTotal = document.querySelector('#btn__proximo--duracaoTotal');
 
@@ -18,7 +33,16 @@ capturarDuracaoTotal.addEventListener('click', function(e) {
 });
 
 /*variacaoDeTemperatura*/
+var variacaoDeTemperatura = document.querySelector('input[name="variacaoDeTemperatura"]:checked').value;
+var capturarVariacaoDeTemperatura = document.querySelector('#btn__proximo--variacaoDeTemperatura');
 
+capturarVariacaoDeTemperatura.addEventListener('click', function(e) {
+    alert("A temperatura vai permanecer entre" + variacaoDeTemperatura);
+    
+    e.preventDefault();
+});
+
+/*duracaoTemperatura*/
 /*var duracaoTemperatura = document.querySelector('#duracaoTemperatura');
 var capturarDuracaoTemperatura = document.querySelector('#btn__proximo--duracaoTemperatura');
 
@@ -32,6 +56,7 @@ capturarDuracaoTemperatura.addEventListener('click', function(e) {
 
 /*evento*/
 
+/*algoMais*/
 var algoMais = document.querySelector('#algoMais');
 var capturarAlgoMais = document.querySelector('#btn__proximo--algoMais');
 
