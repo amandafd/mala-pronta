75d7dfa9a7177906d1a1dae2562c14e74593e496
amandafd <amanda_dionisio@hotmail.com>
2022-11-11 16:27:27 -0300

    refactor: refatorando função que adiciona quantidade de pijamas e lingeries

--- assets/js/teste.js
+++ assets/js/teste.js
@@ -17,36 +17,28 @@ mostraNome();
 
 
 
-
-
 /*CAPTANDO DURAÇÃO TOTAL DA VIAGEM E FAZENDO CALCULOS DOS ITENS COM BASE APENAS NESSA VARIÁVEL*/
-/*var duracaoTotal = document.querySelector('.formulario__questao--duracaoTotal');
-
-
-duracaoTotal.addEventListener("submit", (e) => {
-    e.preventDefault();
-
-
-});
-
+const formDuracaoTotal = document.querySelector('.formulario__questao--duracaoTotal');
 var quantidadeLingerie = document.querySelector('.quantidade__lingerie');
 var quantidadePijama = document.querySelector('.quantidade__pijama');
-function adicionaItensFixos() {
-    capturarDuracaoTotal.addEventListener('click', function(e) {
-        if (duracaoTotal.value < 3) {
-            quantidadeLingerie.innerHTML = parseInt(duracaoTotal.value) + 2
-            quantidadePijama.innerHTML = 1;
-        } else if(duracaoTotal.value < 15) {
-        quantidadeLingerie.innerHTML = parseInt(duracaoTotal.value) + 2;
-        quantidadePijama.innerHTML = parseInt(parseInt(duracaoTotal.value) / 3);
-    } else {
-        quantidadePijama.innerHTML = 4;
-        quantidadeLingerie.innerHTML = 18;
-    }
 
-    
+function adicionaItensFixos() {
+    formDuracaoTotal.addEventListener("submit", (e) => {
         e.preventDefault();
-    });
+        
+        const duracaoTotal = parseInt(e.target.elements['duracaoTotal'].value);
+        
+        if(duracaoTotal < 4) {
+            quantidadeLingerie.innerHTML = duracaoTotal + 2;
+            quantidadePijama.innerHTML = 1;
+        } else if (duracaoTotal < 15) {
+            quantidadeLingerie.innerHTML = duracaoTotal + 2;
+            quantidadePijama.innerHTML = parseInt(duracaoTotal / 2);    
+        } else {
+            quantidadePijama.innerHTML = 5;
+            quantidadeLingerie.innerHTML = 18;
+        }
+    })
 }
 
-adicionaItensFixos();*/
+adicionaItensFixos();
