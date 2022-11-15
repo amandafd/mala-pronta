34bba4983952d2df6d3c50ee46a9a402fe09f947
amandafd <amanda_dionisio@hotmail.com>
2022-11-15 18:25:18 -0300

    refactor: refatorando javascript

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,8 +1,113 @@
-/*CAPTANDO NOME*/ 
+/*CAPTANDO FORMS*/
 const formNome = document.querySelector('.formulario__questao--nome');
+const formDuracaoTotal = document.querySelector('.formulario__questao--duracaoTotal');
+const formVariacaoTemperatura = document.querySelector('.formulario__questao--variacaoDeTemperatura');
+const formCenario = document.querySelector('.formulario__questao--cenario');
+const formEvento = document.querySelector('.formulario__questao--evento');
+const formAlgoMais = document.querySelector('.formulario__questao--algoMais');
+
+/*CAPTANDO INPUTS*/
+const t25mais = document.querySelector('#vdt--25mais');
+const t20e25 = document.querySelector('#vdt--20e25');
+const t15e20 = document.querySelector('#vdt--15e20');
+const t10e15 = document.querySelector('#vdt--10e15');
+const t5e10 = document.querySelector('#vdt--5e10');
+const t0e5 = document.querySelector('#vdt--0e5');
+const tNegativa = document.querySelector('#vdt--negativa');
+
+const t25maisDuracao = document.querySelector('#vdt__selecionar--25mais');
+const t20e25Duracao = document.querySelector('#vdt__selecionar--20e25');
+const t15e20Duracao = document.querySelector('#vdt__selecionar--15e20');
+const t10e15Duracao = document.querySelector('#vdt__selecionar--10e15');
+const t5e10Duracao = document.querySelector('#vdt__selecionar--5e10');
+const t0e5Duracao = document.querySelector('#vdt__selecionar--0e5');
+const tNegativaDuracao = document.querySelector('#vdt__selecionar--negativa');
+
+const agua = document.querySelector('#checkbox--agua');
+const neve = document.querySelector('#checkbox--neve');
+const montanha = document.querySelector('#checkbox--montanha');
+const cidade = document.querySelector('#checkbox--cidade');
+
+const nada = document.querySelector('.checkbox--nada');
+const balada = document.querySelector('.checkbox--balada');
+const fantasia = document.querySelector('.checkbox--fantasia');
+const formal = document.querySelector('.checkbox--formal');
+
+/*CAPTANDO ITENS ESCONDIDOS*/
+const linho = document.querySelector('.linho'); 
+const seda = document.querySelector('.seda'); 
+const cetim = document.querySelector('.cetim'); 
+const malha = document.querySelector('.malha'); 
+const jeans = document.querySelector('.jeans'); 
+const couro = document.querySelector('.couro'); 
+const veludo = document.querySelector('.veludo'); 
+const la = document.querySelector('.la'); 
+const pelagem = document.querySelector('.pelagem'); 
+
+const camisetaMC = document.querySelector('.camisetaMC'); 
+const camisetaML = document.querySelector('.camisetaML'); 
+const camisa = document.querySelector('.camisa'); 
+const shorts = document.querySelector('.shorts'); 
+const calcaCVL = document.querySelector('.calcaCVL'); 
+const cardiga = document.querySelector('.cardiga'); 
+const jaqueta = document.querySelector('.jaqueta'); 
+const casaco = document.querySelector('.casaco'); 
+
+const tipoCalcaTecido = document.querySelector('.tipoCalcaTecido'); 
+const shortsSubS = document.querySelector('.shortsSubS');
+
+const sandalia = document.querySelector('.sandalia'); 
+const bota = document.querySelector('.bota');
+
+const cachecol = document.querySelector('.cachecol'); 
+const meiaCalcaFina = document.querySelector('.meiaCalcaFina'); 
+const meiaCalcaMedia = document.querySelector('.meiaCalcaMedia'); 
+const meiaCalcaGrossa = document.querySelector('.meiaCalcaGrossa'); 
+const gorro = document.querySelector('.gorro'); 
+const luva = document.querySelector('.luva'); 
+
+/*CAPTANDO TEXTO QUE SERÃO SUBSTITUIDO*/
 const nomeMala = document.querySelector('.mala__nome');
 const textoInicialMala = document.querySelector('.mala__textoInicial');
 
+/*CAPTANDO QUANTIDADE DE ITENS*/
+const quantidadeLingerie = document.querySelector('.quantidade__lingerie'); 
+const quantidadeMeia = document.querySelector('.quantidade__meia'); 
+const quantidadePijama = document.querySelector('.quantidade__pijama'); 
+const quantidadeVestidoMacacao = document.querySelector('.quantidade__vestidoMacacao'); 
+const quantidadePtCimaSubV = document.querySelector('.quantidade__ptCimaSub'); 
+const quantidadePtBaixoSubV = document.querySelector('.quantidade__ptBaixoSub'); 
+const quantidadeCamisetaMC = document.querySelector('.quantidade__camisetaMC'); 
+const quantiadeCamisetaML = document.querySelector('.quantidade__camisetaML'); 
+const quantidadeCamisa = document.querySelector('.quantidade__camisa'); 
+const quantidadeShots = document.querySelector('.quantidade__shorts'); 
+const quantidadeCalcaTecido = document.querySelector('.quantidade__calcaTecido'); 
+const quantidadeCalcaJeans = document.querySelector('.quantidade__calcaJeans'); 
+const quantidadeCalcaCVL = document.querySelector('.quantidade__calcaCVL'); 
+const quantidadeSaia = document.querySelector('.quantidade__saia'); 
+const quantidadeMoletom = document.querySelector('.quantidade__moletom'); 
+const quantidadeCardiga = document.querySelector('.quantidade__cardiga'); 
+const quantidadeJaqueta = document.querySelector('.quantidade__jaqueta'); 
+const quantidadeCasaco = document.querySelector('.quantidade__casaco'); 
+
+const quantidadeChinelo = document.querySelector('.quantidade__chinelo'); 
+const quantidadeSandalia = document.querySelector('.quantidade__sandalia'); 
+const quantidadeTenis = document.querySelector('.quantidade__tenis'); 
+
+const quantidadeOculosSol = document.querySelector('.quantidade__oculosSol'); 
+const quantidadeLenco = document.querySelector('.quantidade__lenco'); 
+const quantidadeCachecol = document.querySelector('.quantidade__cachecol'); 
+const quantidadeMeiaCalcaFina = document.querySelector('.quantidade__meiaCalcaFina'); 
+const quantidadeMeiaCalcaMedia = document.querySelector('.quantidade__meiaCalcaMedia'); 
+const quantidadeMeiaCalcaGrossa = document.querySelector('.quantidade__meiaCalcaGrossa'); 
+const quantidadeGorro = document.querySelector('.quantidade__gorro'); 
+const quantidadeLuva = document.querySelector('.quantidade__luva'); 
+
+const quantidadeToalhaBanho = document.querySelector('.quantidade__toalhaBanho'); 
+const quantidadeToalhaPiscina = document.querySelector('.quantidade__toalhaPiscina'); 
+
+
+/*CAPTANDO NOME*/ 
 function mostraNome() {
     formNome.addEventListener("submit", (e) => {
         e.preventDefault();
@@ -15,10 +120,6 @@ function mostraNome() {
 mostraNome();
 
 /*CAPTANDO DURAÇÃO TOTAL DA VIAGEM E FAZENDO CALCULOS DOS ITENS COM BASE APENAS NESSA VARIÁVEL*/
-const formDuracaoTotal = document.querySelector('.formulario__questao--duracaoTotal');
-var quantidadeLingerie = document.querySelector('.quantidade__lingerie');
-var quantidadePijama = document.querySelector('.quantidade__pijama');
-
 function adicionaItensFixos() {
     formDuracaoTotal.addEventListener("submit", (e) => {
         e.preventDefault();
@@ -40,56 +141,7 @@ function adicionaItensFixos() {
 
 adicionaItensFixos();
 
-/*CAPTANDO VARIAÇÔES DE TEMPERATURA SELECIONADAS E ATIVANDO INPUTS PARA DURAÇÃO DESSAS VARIAÇÕESS*/
-const formVariacaoTemperatura = document.querySelector('.formulario__questao--variacaoDeTemperatura');
-
-const t25mais = document.querySelector('#vdt--25mais');
-const t20e25 = document.querySelector('#vdt--20e25');
-const t15e20 = document.querySelector('#vdt--15e20');
-const t10e15 = document.querySelector('#vdt--10e15');
-const t5e10 = document.querySelector('#vdt--5e10');
-const t0e5 = document.querySelector('#vdt--0e5');
-const tNegativa = document.querySelector('#vdt--negativa');
-
-const t25maisDuracao = document.querySelector('#vdt__selecionar--25mais');
-const t20e25Duracao = document.querySelector('#vdt__selecionar--20e25');
-const t15e20Duracao = document.querySelector('#vdt__selecionar--15e20');
-const t10e15Duracao = document.querySelector('#vdt__selecionar--10e15');
-const t5e10Duracao = document.querySelector('#vdt__selecionar--5e10');
-const t0e5Duracao = document.querySelector('#vdt__selecionar--0e5');
-const tNegativaDuracao = document.querySelector('#vdt__selecionar--negativa');
-
-const linho = document.querySelector('.linho');
-const seda = document.querySelector('.seda');
-const cetim = document.querySelector('.cetim');
-const malha = document.querySelector('.malha');
-const jeans = document.querySelector('.jeans');
-const couro = document.querySelector('.couro');
-const veludo = document.querySelector('.veludo');
-const la = document.querySelector('.la');
-const pelagem = document.querySelector('.pelagem');
-
-const camisetaMC = document.querySelector('.camisetaMC');
-const camisetaML = document.querySelector('.camisetaML');
-const croppedCamisa = document.querySelector('.croppedCamisa');
-const bermudaShorts = document.querySelector('.bermudaShorts');
-const calcaCVL = document.querySelector('.calcaCVL');
-const cardigaSueter = document.querySelector('.cardigaSueter');
-const jaqueta = document.querySelector('.jaqueta');
-const casaco = document.querySelector('.casaco');
-const sandalia = document.querySelector('.sandalia');
-const bota = document.querySelector('.bota');
-const meiaCalcaFina = document.querySelector('.meiaCalcaFina');
-const meiaCalcaMedia = document.querySelector('.meiaCalcaMedia');
-const meiaCalcaGrossa = document.querySelector('.meiaCalcaGrossa');
-const gorro = document.querySelector('.gorro');
-const luva = document.querySelector('.luva');
-const cachecol = document.querySelector('.cachecol');
-
-const tipoCalcaTecido = document.querySelector('.tipoCalcaTecido');
-const bermudaShortsSub2 = document.querySelector('.bermudaShortsSub2');
-
-
+/*CAPTANDO VARIAÇÔES DE TEMPERATURA SELECIONADAS*/
 function verificaVariacaoTemperatura() {
     formVariacaoTemperatura.addEventListener("submit", (e) => {
         e.preventDefault();
@@ -125,9 +177,9 @@ function verificaVariacaoTemperatura() {
                 malha.hidden = false;
 
                 camisetaMC.hidden = false;
-                bermudaShorts.hidden = false;
+                shorts.hidden = false;
                 tipoCalcaTecido.hidden = false;
-                bermudaShortsSub2.hidden = false;
+                shortsSubS.hidden = false;
                 sandalia.hidden = false;
             }
             if(t20e25.checked == true) {
@@ -138,9 +190,9 @@ function verificaVariacaoTemperatura() {
                 jeans.hidden = false;
 
                 camisetaMC.hidden = false;
-                bermudaShorts.hidden = false;
+                shorts.hidden = false;
                 tipoCalcaTecido.hidden = false;
-                bermudaShortsSub2.hidden = false;
+                shortsSubS.hidden = false;
                 sandalia.hidden = false;
                 meiaCalcaFina.hidden = false;
             }
@@ -151,7 +203,7 @@ function verificaVariacaoTemperatura() {
 
                 camisetaMC.hidden = false;
                 camisetaML.hidden = false;
-                cardigaSueter.hidden = false;
+                cardiga.hidden = false;
                 jaqueta.hidden = false;
                 sandalia.hidden = false;
                 meiaCalcaFina.hidden = false;
@@ -165,7 +217,7 @@ function verificaVariacaoTemperatura() {
 
                 camisetaMC.hidden = false;
                 camisetaML.hidden = false;
-                cardigaSueter.hidden = false;
+                cardiga.hidden = false;
                 jaqueta.hidden = false;
                 casaco.hidden = false;
                 bota.hidden = false;
@@ -180,7 +232,7 @@ function verificaVariacaoTemperatura() {
 
                 camisetaML.hidden = false;
                 calcaCVL.hidden = false;
-                cardigaSueter.hidden = false;
+                cardiga.hidden = false;
                 jaqueta.hidden = false;
                 casaco.hidden = false;
                 bota.hidden = false;
@@ -198,7 +250,7 @@ function verificaVariacaoTemperatura() {
 
                 camisetaML.hidden = false;
                 calcaCVL.hidden = false;
-                cardigaSueter.hidden = false;
+                cardiga.hidden = false;
                 jaqueta.hidden = false;
                 casaco.hidden = false;
                 bota.hidden = false;
@@ -216,7 +268,7 @@ function verificaVariacaoTemperatura() {
 
                 camisetaML.hidden = false;
                 calcaCVL.hidden = false;
-                cardigaSueter.hidden = false;
+                cardiga.hidden = false;
                 jaqueta.hidden = false;
                 casaco.hidden = false;
                 bota.hidden = false;
@@ -235,36 +287,26 @@ function verificaVariacaoTemperatura() {
 verificaVariacaoTemperatura();
 
 
-
-/*duracaoTemperatura*/
-/*const formDuracaoTemperatura = document.querySelector('.formulario__questao--duracaoTemperatura');
-
-function verificaDuracaoTemperatura() {
-    formDuracaoTemperatura.addEventListener("submit", (e) => {
-        e.preventDefault();
-
-        console.assert.log(e);
-    })
-}
-
-verificaDuracaoTemperatura();*/
-
 /*cenario*/
-/*const formCenario = document.querySelector('.formulario__questao--cenario');
-
 function verificaCenario() {
     formCenario.addEventListener("submit", (e) => {
         e.preventDefault();
 
-        console.assert.log(e);
+        if(agua.checked == true) {
+            console.log("Água");
+        }
+        if(neve.checked == true) {
+            console.log("Neve");
+        }
+        if(montanha.checked == true) {
+            console.log("Montanha");
+        }
     })
 }
 
-verificaCenario();*/
+verificaCenario();
 
 /*evento*/
-/*const formEvento = document.querySelector('.formulario__questao--evento');
-
 function verificaEvento() {
     formEvento.addEventListener("submit", (e) => {
         e.preventDefault();
@@ -273,17 +315,15 @@ function verificaEvento() {
     })
 }
 
-verificaEvento();*/
+verificaEvento();
 
 /*algoMais*/
-/*const formAlgoMais = document.querySelector('.formulario__questao--algoMais');
-
 function verificaAlgoMais() {
     formAlgoMais.addEventListener("submit", (e) => {
         e.preventDefault();
 
-        console.assert.log(e);
+        textoInicialMala.innerHTML = "Tudo certo! Verifique sua mala pronta abaixo.";
     })
 }
 
-verificaAlgoMais();*/
+verificaAlgoMais();
