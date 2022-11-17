afa555d31c4904a03742f5899a17586cbf289737
amandafd <amanda_dionisio@hotmail.com>
2022-11-17 18:18:27 -0300

    refactor: refatorando função que mostra tecidos. Existe uma possibilidade de reaftoração deixada comentada para verificar qual a melhor opção

--- assets/js/script.js
+++ assets/js/script.js
@@ -127,7 +127,6 @@ adicionaItensFixos();
 
 
 /*CAPTANDO VARIAÇÔES DE TEMPERATURA SELECIONADAS*/
-
 const formVariacaoTemperatura = document.querySelector('.formulario__questao--variacaoDeTemperatura');
 
 const t25maisDuracao = document.querySelector('#vdt__selecionar--25mais');
@@ -137,6 +136,14 @@ const t10e15Duracao = document.querySelector('#vdt__selecionar--10e15');
 const t5e10Duracao = document.querySelector('#vdt__selecionar--5e10');
 const t0e5Duracao = document.querySelector('#vdt__selecionar--0e5');
 const tNegativaDuracao = document.querySelector('#vdt__selecionar--negativa');
+
+const tecido25mais = document.querySelectorAll('.tecido25mais');
+const tecido20e25 = document.querySelectorAll('.tecido20e25');
+const tecido15e20 = document.querySelectorAll('.tecido15e20');
+const tecido10e15 = document.querySelectorAll('.tecido10e15');
+const tecidoMenosDe10 = document.querySelectorAll('.tecidoMenosDe10')
+
+const tecido = document.querySelectorAll('.tecido');
 
 function verificaVariacaoTemperatura() {
     formVariacaoTemperatura.onsubmit = function(e) {
@@ -159,39 +166,63 @@ function verificaVariacaoTemperatura() {
                 t10e15Duracao.disabled = false;
                 t10e15Duracao.required = true;  
             }
-            if(e.target[4].checked== true) {
+            if(e.target[5].checked== true) {
                 t5e10Duracao.disabled = false;
                 t5e10Duracao.required = true;  
             }
-            if(e.target[5].checked == true) {
+            if(e.target[6].checked == true) {
                 t0e5Duracao.disabled = false;
                 t0e5Duracao.required = true;  
             }
-            if(e.target[6].checked == true) {
+            if(e.target[7].checked == true) {
                 tNegativaDuracao.disabled = false;
                 tNegativaDuracao.required = true;  
             }
         }
-
         function mostraTecidos() {
-            if(t25mais.checked == true) {
-                linho.hidden = false;
-                seda.hidden = false; 
-                malha.hidden = false;
-
+            /*for(i = 0; i < tecido.length; i++) {
+                if(e.target[1].checked == true) {
+                    tecido[1].hidden = false;
+                }
+            }*/
+            for(i = 0; i < tecido25mais.length; i++) {
+                if(e.target[1].checked == true) {
+                    tecido25mais[i].hidden = false;
+                }
+            }
+            for(i = 0; i < tecido20e25.length; i++) {
+                if(e.target[2].checked == true) {
+                    tecido20e25[i].hidden = false;
+                }
+            }
+            for(i = 0; i < tecido15e20.length; i++) {
+                if(e.target[3].checked == true) {
+                    tecido15e20[i].hidden = false;
+                }
+            }
+            for(i = 0; i < tecido10e15.length; i++) {
+                if(e.target[4].checked == true) {
+                    tecido10e15[i].hidden = false;
+                }
+            }
+            for(i = 0; i < tecidoMenosDe10.length; i++) {
+                if(e.target[5].checked == true || e.target[6].checked == true || e.target[7].checked == true) {
+                    tecidoMenosDe10[i].hidden = false;
+                }
+            }
+            /*for(i = 0; i < tecido15e20.length; i++) {}
+            for(i = 0; i < tecido10e15.length; i++) {}
+            for(i = 0; i < tecido5e10.length; i++) {}
+            for(i = 0; i < tecido0e5.length; i++) {}
+            for(i = 0; i < tecidoNegativa.length; i++) {}*/
+            /*if(e.target[1].checked == true) {
                 camisetaMC.hidden = false;
                 shorts.hidden = false;
                 tipoCalcaTecido.hidden = false;
                 shortsSubS.hidden = false;
                 sandalia.hidden = false;
             }
-            if(t20e25.checked == true) {
-                linho.hidden = false;
-                seda.hidden = false;
-                cetim.hidden = false;
-                malha.hidden = false;
-                jeans.hidden = false;
-
+            if(e.target[2].checked == true) {
                 camisetaMC.hidden = false;
                 shorts.hidden = false;
                 tipoCalcaTecido.hidden = false;
@@ -199,11 +230,7 @@ function verificaVariacaoTemperatura() {
                 sandalia.hidden = false;
                 meiaCalcaFina.hidden = false;
             }
-            if(t15e20.checked == true) {
-                jeans.hidden = false;
-                couro.hidden = false;
-                veludo.hidden = false;
-
+            if(e.target[3].checked == true) {
                 camisetaMC.hidden = false;
                 camisetaML.hidden = false;
                 cardiga.hidden = false;
@@ -212,12 +239,7 @@ function verificaVariacaoTemperatura() {
                 meiaCalcaFina.hidden = false;
                 meiaCalcaMedia.hidden = false;
             }
-            if(t10e15.checked == true) {
-                jeans.hidden = false;
-                couro.hidden = false;
-                veludo.hidden = false;
-                la.hidden = false;
-
+            if(e.target[4].checked == true) {
                 camisetaMC.hidden = false;
                 camisetaML.hidden = false;
                 cardiga.hidden = false;
@@ -227,12 +249,7 @@ function verificaVariacaoTemperatura() {
                 meiaCalcaMedia.hidden = false;
                 cachecol.hidden = false;
             }
-            if(t5e10.checked == true ) {
-                jeans.hidden = false;
-                couro.hidden = false;
-                veludo.hidden = false;
-                pelagem.hidden = false;
-
+            if(e.target[5].checked == true ) {
                 camisetaML.hidden = false;
                 calcaCVL.hidden = false;
                 cardiga.hidden = false;
@@ -245,12 +262,7 @@ function verificaVariacaoTemperatura() {
                 luva.hidden = false;
                 cachecol.hidden = false;
             }
-            if(t0e5.checked == true) {
-                jeans.hidden = false;
-                couro.hidden = false;
-                veludo.hidden = false;
-                pelagem.hidden = false;
-
+            if(e.target[6].checked == true) {
                 camisetaML.hidden = false;
                 calcaCVL.hidden = false;
                 cardiga.hidden = false;
@@ -263,12 +275,7 @@ function verificaVariacaoTemperatura() {
                 luva.hidden = false;
                 cachecol.hidden = false;
             }
-            if(tNegativa.checked == true) {
-                jeans.hidden = false;
-                couro.hidden = false;
-                veludo.hidden = false;
-                pelagem.hidden = false;
-
+            if(e.target[7].checked == true) {
                 camisetaML.hidden = false;
                 calcaCVL.hidden = false;
                 cardiga.hidden = false;
@@ -279,7 +286,7 @@ function verificaVariacaoTemperatura() {
                 gorro.hidden = false;
                 luva.hidden = false;
                 cachecol.hidden = false;
-            }
+            }*/
         }
         ativaInputs();
         mostraTecidos();
--- assets/js/teste.js
+++ assets/js/teste.js
@@ -53,6 +53,14 @@ const t5e10Duracao = document.querySelector('#vdt__selecionar--5e10');
 const t0e5Duracao = document.querySelector('#vdt__selecionar--0e5');
 const tNegativaDuracao = document.querySelector('#vdt__selecionar--negativa');
 
+const tecido25mais = document.querySelectorAll('.tecido25mais');
+const tecido20e25 = document.querySelectorAll('.tecido20e25');
+const tecido15e20 = document.querySelectorAll('.tecido15e20');
+const tecido10e15 = document.querySelectorAll('.tecido10e15');
+const tecidoMenosDe10 = document.querySelectorAll('.tecidoMenosDe10')
+
+const tecido = document.querySelectorAll('.tecido');
+
 function verificaVariacaoTemperatura() {
     formVariacaoTemperatura.onsubmit = function(e) {
         e.preventDefault();
@@ -74,20 +82,130 @@ function verificaVariacaoTemperatura() {
                 t10e15Duracao.disabled = false;
                 t10e15Duracao.required = true;  
             }
-            if(e.target[4].checked== true) {
+            if(e.target[5].checked== true) {
                 t5e10Duracao.disabled = false;
                 t5e10Duracao.required = true;  
             }
-            if(e.target[5].checked == true) {
+            if(e.target[6].checked == true) {
                 t0e5Duracao.disabled = false;
                 t0e5Duracao.required = true;  
             }
-            if(e.target[6].checked == true) {
+            if(e.target[7].checked == true) {
                 tNegativaDuracao.disabled = false;
                 tNegativaDuracao.required = true;  
             }
         }
+        function mostraTecidos() {
+            /*for(i = 0; i < tecido.length; i++) {
+                if(e.target[1].checked == true) {
+                    tecido[1].hidden = false;
+                }
+            }*/
+            for(i = 0; i < tecido25mais.length; i++) {
+                if(e.target[1].checked == true) {
+                    tecido25mais[i].hidden = false;
+                }
+            }
+            for(i = 0; i < tecido20e25.length; i++) {
+                if(e.target[2].checked == true) {
+                    tecido20e25[i].hidden = false;
+                }
+            }
+            for(i = 0; i < tecido15e20.length; i++) {
+                if(e.target[3].checked == true) {
+                    tecido15e20[i].hidden = false;
+                }
+            }
+            for(i = 0; i < tecido10e15.length; i++) {
+                if(e.target[4].checked == true) {
+                    tecido10e15[i].hidden = false;
+                }
+            }
+            for(i = 0; i < tecidoMenosDe10.length; i++) {
+                if(e.target[5].checked == true || e.target[6].checked == true || e.target[7].checked == true) {
+                    tecidoMenosDe10[i].hidden = false;
+                }
+            }
+            /*for(i = 0; i < tecido15e20.length; i++) {}
+            for(i = 0; i < tecido10e15.length; i++) {}
+            for(i = 0; i < tecido5e10.length; i++) {}
+            for(i = 0; i < tecido0e5.length; i++) {}
+            for(i = 0; i < tecidoNegativa.length; i++) {}*/
+            /*if(e.target[1].checked == true) {
+                camisetaMC.hidden = false;
+                shorts.hidden = false;
+                tipoCalcaTecido.hidden = false;
+                shortsSubS.hidden = false;
+                sandalia.hidden = false;
+            }
+            if(e.target[2].checked == true) {
+                camisetaMC.hidden = false;
+                shorts.hidden = false;
+                tipoCalcaTecido.hidden = false;
+                shortsSubS.hidden = false;
+                sandalia.hidden = false;
+                meiaCalcaFina.hidden = false;
+            }
+            if(e.target[3].checked == true) {
+                camisetaMC.hidden = false;
+                camisetaML.hidden = false;
+                cardiga.hidden = false;
+                jaqueta.hidden = false;
+                sandalia.hidden = false;
+                meiaCalcaFina.hidden = false;
+                meiaCalcaMedia.hidden = false;
+            }
+            if(e.target[4].checked == true) {
+                camisetaMC.hidden = false;
+                camisetaML.hidden = false;
+                cardiga.hidden = false;
+                jaqueta.hidden = false;
+                casaco.hidden = false;
+                bota.hidden = false;
+                meiaCalcaMedia.hidden = false;
+                cachecol.hidden = false;
+            }
+            if(e.target[5].checked == true ) {
+                camisetaML.hidden = false;
+                calcaCVL.hidden = false;
+                cardiga.hidden = false;
+                jaqueta.hidden = false;
+                casaco.hidden = false;
+                bota.hidden = false;
+                meiaCalcaMedia.hidden = false;
+                meiaCalcaGrossa.hidden = false;
+                gorro.hidden = false;
+                luva.hidden = false;
+                cachecol.hidden = false;
+            }
+            if(e.target[6].checked == true) {
+                camisetaML.hidden = false;
+                calcaCVL.hidden = false;
+                cardiga.hidden = false;
+                jaqueta.hidden = false;
+                casaco.hidden = false;
+                bota.hidden = false;
+                meiaCalcaMedia.hidden = false;
+                meiaCalcaGrossa.hidden = false;
+                gorro.hidden = false;
+                luva.hidden = false;
+                cachecol.hidden = false;
+            }
+            if(e.target[7].checked == true) {
+                camisetaML.hidden = false;
+                calcaCVL.hidden = false;
+                cardiga.hidden = false;
+                jaqueta.hidden = false;
+                casaco.hidden = false;
+                bota.hidden = false;
+                meiaCalcaGrossa.hidden = false;
+                gorro.hidden = false;
+                luva.hidden = false;
+                cachecol.hidden = false;
+            }*/
+        }
         ativaInputs();
+        mostraTecidos();
     }
 }
 
